91bd1f6f8aea2bed090a95779730be6cca259f8b
Kim Hayeon <54922913+kimhayeon00@users.noreply.github.com>
2022-01-24 15:08:29 +0900

    Update 모험가 길드.py

--- Greedy/모험가 길드/모험가 길드.py
+++ Greedy/모험가 길드/모험가 길드.py
@@ -1,8 +1,11 @@
 N = int(input())
 lst = list(map(int, input().split()))
 cnt = 0
+tmp = 0
 for i in range(N+1):
-    if lst.count(i+1) >= i+1 :
-        cnt += lst.count(i+1)//(i+1)
+    A = lst.count(i+1)+tmp
+    if A >= i+1 :
+        cnt += A//(i+1)
+        tmp = A%(i+1)
     
 print(cnt);
